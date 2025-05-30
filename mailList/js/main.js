e4353b38eb7019a6307426033b6c4cc61978e274
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 18:27:50 +0200

    Refactor email check logic in main.js for clarity and maintainability

--- mailList/js/main.js
+++ mailList/js/main.js
@@ -22,19 +22,20 @@ let inList = false;
 
 let userMail = prompt("Benvenuto alla Festa! Inserisci la tua e-mail");
 
+// todo creo un ciclo for che controlli se l'e-mail inserita è presente nella lista da me creata
 // * inizializzo il ciclo for per verificare la presenza in inviteList della userMail
 
 for (i = 0; i < inviteList.length; i++) {
   if (userMail === inviteList[i]) {
-    inList = true;
-    console.log("Sei in lista!");
-    break;
+    // controllo se l'userMail trova riscontro negli elementi dell'inviteList
+    // todo se l'email è presente, stampa un messaggio in console e un alert
+    inList = true; // se lo trova, inList diventa true
+    console.log("Sei in lista!"); // quindi stampa il messaggio di conferma
+    break; // chiudo il ciclo
   }
 }
-if (inList == false) {
-  console.log("Non sei in lista, ci dispiace!");
-}
-
-// todo creo un ciclo for che controlli se l'e-mail inserita è presente nella lista da me creata
-// todo se l'email è presente, stampa un messaggio in console e un alert
 // ? se non è presente stampa un messaggio differente
+if (inList == false) {
+  // se non trova riscontro inList = False
+  console.log("Non sei in lista, ci dispiace!"); // stampo il messaggio di failure
+}
