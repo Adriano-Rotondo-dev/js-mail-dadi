10351f936f348b67f47ce412e058466be6287646
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 18:36:41 +0200

    Add detailed comments for random number generation and game logic in main.js

--- diceGame/js/main.js
+++ diceGame/js/main.js
@@ -1,2 +1,14 @@
 // TODO Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // ! Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+// * creo un RNG prompt da 1 a 6 per l'user 
+
+// * credp un RNG per il computer 
+
+// * paragono i due numeri usciti 
+
+// * il più alto "vince" 
+
+// * se vince il numero generato dal giocatore, stampo il messaggio e l'alert di vittoria dello user
+
+// * se vince il numero generato dal computer, stampo il messaggio e l'alert di sconfitta dello user
