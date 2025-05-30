e9104b0da8f2cdc3c684d5e1d8a5843fe2bf5d4c
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 18:20:03 +0200

    Fix inviteList by removing duplicate entries and ensure user email prompt functionality

--- mailList/js/main.js
+++ mailList/js/main.js
@@ -7,14 +7,34 @@
 
 const inviteList = [
   "gianni@boolean.com",
-  "@boolean.com",
-  "@boolean.com",
-  "@boolean.com",
-  "@boolean.com",
-  "@boolean.com",
+  "franco@boolean.com",
+  "peppe@boolean.com",
+  "lillo@boolean.com",
+  "nanni@boolean.com",
+  "gigi@boolean.com",
 ];
 
-// todo creo un prompt che chieda agli invitati di inserire la propria e-mail
+// * creo un boolean per poi verificare la presenza nella lista email
+
+let inList = false;
+
+// * creo un prompt che chieda agli invitati di inserire la propria e-mail
+
+let userMail = prompt("Benvenuto alla Festa! Inserisci la tua e-mail");
+
+// * inizializzo il ciclo for per verificare la presenza in inviteList della userMail
+
+for (i = 0; i < inviteList.length; i++) {
+  if (userMail === inviteList[i]) {
+    inList = true;
+    console.log("Sei in lista!");
+    break;
+  }
+}
+if (inList == false) {
+  console.log("Non sei in lista, ci dispiace!");
+}
+
 // todo creo un ciclo for che controlli se l'e-mail inserita è presente nella lista da me creata
 // todo se l'email è presente, stampa un messaggio in console e un alert
 // ? se non è presente stampa un messaggio differente
