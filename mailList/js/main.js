9916682acd2f6ec35f721178c19aa47e1f95c13c
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-30 16:37:04 +0200

    Add index.html and main.js files for Dice Game and update mailList files for consistency

--- mailList/js/main.js
+++ mailList/js/main.js
@@ -1,10 +1,10 @@
-// TODO Mail - Crea una lista di email di invitati ad una festa. 
+// TODO Mail - Crea una lista di email di invitati ad una festa.
 // TODO Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
-// ! Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’. 
-// !  Non è necessario provvedere alla validazione delle email 
+// ! Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’.
+// !  Non è necessario provvedere alla validazione delle email
 
 // TODO  Creo una lista di email degli invitati
 // todo creo un prompt che chieda agli invitati di inserire la propria e-mail
 // todo creo un ciclo for che controlli se l'e-mail inserita è presente nella lista da me creata
-// todo se l'email è presente, stampa un messaggio in console e un alert 
-// ? se non è presente stampa un messaggio differente 
+// todo se l'email è presente, stampa un messaggio in console e un alert
+// ? se non è presente stampa un messaggio differente
